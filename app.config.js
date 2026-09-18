6043814694ac1b5ff1c411dfade04df3d043c13d
Rodolfo Silva <w.rodolfo@outlook.com.br>
2026-09-18 14:16:45 -0300

    chore: configure Google Maps for Android development build

---
+++ app.config.js
@@ -0,0 +1,22 @@
+﻿module.exports = ({ config }) => {
+  const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
+
+  if (!googleMapsApiKey) {
+    throw new Error(
+      'GOOGLE_MAPS_API_KEY is not configured. Set it in EAS or .env.local.'
+    );
+  }
+
+  return {
+    ...config,
+    android: {
+      ...config.android,
+      config: {
+        ...(config.android?.config ?? {}),
+        googleMaps: {
+          apiKey: googleMapsApiKey,
+        },
+      },
+    },
+  };
+};
